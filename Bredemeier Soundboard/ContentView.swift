f52172f4226b022e440baabd9242174159919810
Dhanush Tipparaju <dhanushtips@gmail.com>
2022-11-11 10:30:10 -0600

    pressing images in the grid results in a change in values(later will be audio output)

--- Bredemeier Soundboard/ContentView.swift
+++ Bredemeier Soundboard/ContentView.swift
@@ -8,27 +8,29 @@
 import SwiftUI
 
 struct ContentView: View {
-    
+    @State private var pressed = ""
+    @State private var count = 0 //using this to test to see if the pressing of images work
     var body: some View {
-            VStack {
-                //add image of mr. b for the app icon(preferably funny)
-                Text("Bredemeier Soundboard")
-                    .font(.system(size: 60))
-                    .padding()
-                LazyVGrid(columns: Array(repeating: GridItem(.fixed(120), spacing: 15), count: 2), spacing: 15) {
-                    Text("x")
-                    Text("x")
-                    Text("x")
-                    Text("x")
-                    Text("x")
-                    Text("x")
-                    Text("x")
-                    Text("x")
-                    Text("x")
-                    Text("x")
+        VStack {
+            //add image of mr. b for the app icon(preferably funny) using pig image for sake of seeing if my stuff works right now
+            Text("Bredemeier Soundboard")
+                .font(.title)
+                .padding()
+            Text("\(count)")
+            LazyVGrid(columns: Array(repeating: GridItem(.fixed(120), spacing: 15), count: 2), spacing: 15) {
+                
+                ForEach(0..<10) { index in
+                    ZStack{
+                        Image("Bredemier")
+                            .resizable()
+                            .frame(width: 100, height: 100)
+                            .onTapGesture { //for now it will add numbers but later make it play audio when pressed
+                                count += 1
+                            }
+                    }
                 }
             }
-        
+        }
         .preferredColorScheme(.dark)
     }
 }
