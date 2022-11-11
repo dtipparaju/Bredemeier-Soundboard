13de4150fcc20ec37ba3110b88c34f905fa9a6e1
Dhanush Tipparaju <dhanushtips@gmail.com>
2022-11-11 09:21:31 -0600

    Initial Commit

---
+++ Bredemeier Soundboard/Bredemeier_SoundboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  Bredemeier_SoundboardApp.swift
+//  Bredemeier Soundboard
+//
+//  Created by Dhanush Tipparaju on 11/11/22.
+//
+
+import SwiftUI
+
+@main
+struct Bredemeier_SoundboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
