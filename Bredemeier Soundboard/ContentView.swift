93a18b60311ae5b468e1529e4c733bf25eb5f923
Dhanush Tipparaju <dhanushtips@gmail.com>
2022-11-18 10:39:12 -0600

    added age verification functionality

--- Bredemeier Soundboard/ContentView.swift
+++ Bredemeier Soundboard/ContentView.swift
@@ -5,12 +5,13 @@
 //  Created by Dhanush Tipparaju on 11/11/22.
 //
 
-// Hi - Daksh
 
 import SwiftUI
 
 struct ContentView: View {
     @State private var name = ""
+    @State private var age = ""
+    @State private var okAge = false
     @State private var soundboardViewButton = false
     var body: some View {
         NavigationView{
@@ -18,7 +19,7 @@ struct ContentView: View {
                 Text("Welcome to the soundboard!")
                     .font(.title2).bold()
                     .padding()
-                Text("What is your name?")
+                Text("What is your name and age?")
                 TextField("Enter name here", text: $name)
                     .textFieldStyle(RoundedBorderTextFieldStyle())
                     .font(.title).bold()
@@ -28,30 +29,53 @@ struct ContentView: View {
                     .onSubmit {
                         soundboardViewButton = true
                     }
+                TextField("Enter age here", text: $age)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .font(.title).bold()
+                    .padding()
+                    .autocapitalization(.none)
+                    .autocorrectionDisabled()
+                    .onChange(of: age, perform: { newValue in
+                        if let value = Int(age) {
+                            if value >= 13 {
+                                okAge = true
+                            }
+                            else {
+                                okAge = false
+                            }
+                        }
+                        else {
+                            age = ""
+                            okAge = false
+                        }
+                    })
                 Image("Bredemeier")
                     .resizable()
-                    .frame(width: 350, height: 350)
+                    .frame(width: 300, height: 300)
                     .clipShape(RoundedRectangle(cornerRadius: 10))
                     .padding()
-                Spacer()
+
+                if soundboardViewButton && okAge == true {
+                            NavigationLink("Lets me see the soundboard!", destination: Soundboard())
+                                .font(.title).bold()
+                }
                 if soundboardViewButton == true {
-                    if name !=  "" {
-                        NavigationLink("Lets me see the soundboard!", destination: Soundboard())
-                            .opacity(1.0)
-                            .font(.title).bold()
+                    if okAge == false {
+                        Text("Sorry \(name), you are too young to use this soundboard.")
+                            .font(.title3)
                     }
-                        
                 }
             }
-            .preferredColorScheme(.dark)
         }
+        .preferredColorScheme(.dark)
     }
 }
-    
-    struct ContentView_Previews: PreviewProvider {
-        static var previews: some View {
-            ContentView()
-        }
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
     }
-    
- 
+}
+
+
